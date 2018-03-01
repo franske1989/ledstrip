8bee70b35b6abbf7c5a9d88551350dc7886fcfee
Frans Jacobs <franske1989@gmail.com>
2018-03-01 23:05:25 +0100

    UPDATE main to work with pi

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include <wiringShift.h>
+
+#define PIN_DATA 0
+#define PIN_CLOCK 14
 
 char r[8] = {0xFF, 0x00, 0x00};
-char g[8] = {0x00, 0xff, 0x00};
+char g[8] = {0x00, 0x0f, 0x00};
 char b[8] = {0x00, 0x00, 0xff};
 char data[4] = {0x94, 0x50, 0x0F, 0xFF};
 int seq = 0;
@@ -10,23 +14,20 @@ int seq = 0;
 void setup()
 {
     wiringPiSetup();
-    //TODO : check pins
-    pinMode(2, OUTPUT); // power
-    pinMode(3, OUTPUT); // SDI
-    pinMode(4, OUTPUT); // SCK
+    pinMode(PIN_DATA, OUTPUT); 	// SDI
+    pinMode(PIN_CLOCK, OUTPUT); // SCK
 }
 
 void writeOut(char out)
 {
-    shiftOut(3, 4, 1, out);
+    shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, out);
 }
 
-int main()
+int main(void)
 {
-
-    for (;;)
+	setup();
+	for(;;)
     {
-        digitalWrite(2, HIGH);
         int i = 0;
         while (i < 8)
         {
@@ -46,7 +47,7 @@ int main()
             i++;
         }
 
-        delay(1000);
+        delay(500);
 
         seq++;
         if (seq == 3)
